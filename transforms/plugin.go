bbfe713f96f62a91f4793539ac10447067d43546
Shawn <smhurley00@gmail.com>
2021-06-03 11:40:34 -0400

    first attempt at runner

--- transforms/plugin.go
+++ transforms/plugin.go
@@ -1,16 +1,17 @@
 package transform
 
 import (
+	jsonpatch "github.com/evanphx/json-patch"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 type Plugin interface {
 	// Determine for a given resources what the plugin is deciding to do with this
-	RunTransform(unstructured.Unstructured) (PluginResponse, error)
+	Run(unstructured.Unstructured) (PluginResponse, error)
 }
 
 type PluginResponse struct {
-	Version    string `json:"version,omitempty"`
-	IsWhiteOut bool   `json:"is_white_out,omitempty"`
-	Patches    []byte `json:"patches,omitempty"`
+	Version    string          `json:"version,omitempty"`
+	IsWhiteOut bool            `json:"isWhiteOut,omitempty"`
+	Patches    jsonpatch.Patch `json:"patches,omitempty"`
 }
--- transforms/runner.go
+++ transforms/runner.go
@@ -1,25 +1,57 @@
 package transform
 
 import (
-	k8stransforms "github.com/konveyor/crane-lib/transforms/kubernetes"
+	"encoding/json"
+
+	jsonpatch "github.com/evanphx/json-patch"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 type Runner struct {
-	AddedAnnotations    map[string]string
-	RegistryReplacement map[string]string
-	RemoveAnnotation    []string
-	NewNamespace        string
+	// This is where we need to put extra info
+	// This should include generic args to be passed to each Plugin
+	// This also needs to handle the options that it will need.
+	// TODO: Figure out options that the runner will need and implement here.
 }
 
-func (r Runner) RunKubernetesTransforms(object unstructured.Unstructured) ([]byte, bool, error) {
-	if k8stransforms.GetWhiteOuts(object.GetObjectKind().GroupVersionKind().GroupKind()) {
+func (r *Runner) Run(object unstructured.Unstructured, plugins []Plugin) ([]byte, bool, error) {
+	haveWhiteOut := false
+	havePatches := false
+	patches := jsonpatch.Patch{}
+	errs := []error{}
+
+	for _, plugin := range plugins {
+		// We want to keep the original while we run each plugin.
+		c := object.DeepCopy()
+		// TODO: Handle Version things here
+		resp, err := plugin.Run(c)
+		if err != nil {
+			//TODO: add debug level logging here
+			errs = append(errs, err)
+			continue
+		}
+		if resp.IsWhiteOut {
+			haveWhiteOut = true
+		}
+		if len(resp.Patches) > 0 {
+			havePatches = true
+			patches = append(patches, resp.Patches)
+		}
+	}
+	// TODO: in the future we should consider a way to speed this up with go routines.
+	if len(errs) > 0 {
+		// TODO: handle error in a reasonable way. Probably needs an enhancement
+		// Should Consider option to ignore errors
+		return nil, false, err[0]
+	}
+	if haveWhiteOut {
+		// TODO: handle if we should skip whiteOut if there is a transform
 		return nil, true, nil
 	}
-
-	t, err := k8stransforms.GetKubernetesTransforms(object, r.AddedAnnotations, r.RegistryReplacement, r.NewNamespace, r.RemoveAnnotation)
-	if err != nil {
-		return nil, false, err
+	if havePatches {
+		// TODO: Handle dedup
+		// TODO: Handle conflicts with paths
+		b, err := json.Marshal(patches)
+		return b
 	}
-	return t, false, nil
 }
