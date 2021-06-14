beb47c81499addc12ea6669b50f115d3bf9742fe
Alay Patel <alay1431@gmail.com>
2021-06-13 23:26:34 -0400

    add utilities for cli custom plugins

---
+++ transform/cli/cli.go
@@ -0,0 +1,70 @@
+package cli
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/konveyor/crane-lib/transform"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+type CustomPlugin struct {
+	// TODO: figure out a way to include the name of the plugin in the error messages.
+	name    string
+	runFunc func(*unstructured.Unstructured) (transform.PluginResponse, error)
+}
+
+func (c *CustomPlugin) Run(u *unstructured.Unstructured) (transform.PluginResponse, error) {
+	if c.runFunc == nil {
+		return transform.PluginResponse{}, nil
+	}
+	return c.runFunc(u)
+}
+
+func NewCustomPlugin(name string, runFunc func(*unstructured.Unstructured) (transform.PluginResponse, error)) transform.Plugin {
+	return &CustomPlugin{
+		name:    name,
+		runFunc: runFunc,
+	}
+}
+
+func Unstructured(reader io.Reader) (*unstructured.Unstructured, error) {
+	decoder := json.NewDecoder(reader)
+	u := &unstructured.Unstructured{}
+	err := decoder.Decode(u)
+	return u, err
+}
+
+func ObjectReaderOrDie() io.Reader {
+	return os.Stdin
+}
+
+func StdOut() io.Writer {
+	return os.Stdout
+}
+
+func StdErr() io.Writer {
+	return os.Stderr
+}
+
+func WriterErrorAndExit(err error) {
+	fmt.Fprintf(StdErr(), err.Error())
+	os.Exit(1)
+}
+
+func RunAndExit(plugin transform.Plugin, u *unstructured.Unstructured) {
+	resp, err := plugin.Run(u)
+	if err != nil {
+		fmt.Fprintf(StdErr(), fmt.Errorf("error when running plugin: %#v", err).Error())
+		os.Exit(1)
+	}
+
+	err = json.NewEncoder(StdOut()).Encode(resp)
+	if err != nil {
+		fmt.Fprintf(StdErr(), fmt.Errorf("error writing plugin response to StdOut: %#v", err).Error())
+		os.Exit(1)
+	}
+}
