00c2c3dbe1058e60f5b70dfb8daeb87cac1615bc
ssingla <ssingla@redhat.com>
2026-08-07 16:57:14 +0530

    add secret cleanup

--- state_transfer/transfer/indirect/cleanup.go
+++ state_transfer/transfer/indirect/cleanup.go
@@ -11,10 +11,12 @@ import (
 
 func (t *IndirectTransfer) Cleanup(ctx context.Context, c client.Client, namespace, pvcName string) error {
 	if len(t.options.Labels) == 0 {
-		return fmt.Errorf("refusing to cleanup with empty labels: would match all pods in namespace %s", namespace)
+		return fmt.Errorf("refusing to cleanup with empty labels: would match all resources in namespace %s", namespace)
 	}
 	cleanupLabels := copyLabels(t.options.Labels)
 	cleanupLabels["app.konveyor.io/created-for-pvc"] = pvcName
+
+	// Delete pods
 	podList := &corev1.PodList{}
 	if err := c.List(ctx, podList,
 		client.InNamespace(namespace),
@@ -26,5 +28,19 @@ func (t *IndirectTransfer) Cleanup(ctx context.Context, c client.Client, namespa
 			return fmt.Errorf("failed to delete pod %s: %w", podList.Items[i].Name, err)
 		}
 	}
+
+	// Delete secrets
+	secretList := &corev1.SecretList{}
+	if err := c.List(ctx, secretList,
+		client.InNamespace(namespace),
+		client.MatchingLabels(cleanupLabels)); err != nil {
+		return fmt.Errorf("failed to list indirect transfer secrets: %w", err)
+	}
+	for i := range secretList.Items {
+		if err := c.Delete(ctx, &secretList.Items[i]); err != nil && !errors.IsNotFound(err) {
+			return fmt.Errorf("failed to delete secret %s: %w", secretList.Items[i].Name, err)
+		}
+	}
+
 	return nil
 }
